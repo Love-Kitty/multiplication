76e21845ec8c5f52d2549b1fd86c20aee642ce95
Slywind <sly.wind.lee@gmail.com>
2020-08-29 16:47:21 +0900

    first commit - page 48

---
+++ src/main/java/microservices/book/multiplication/service/MultiplicationService.java
@@ -0,0 +1,13 @@
+package microservices.book.multiplication.service;
+
+import microservices.book.multiplication.domain.Multiplication;
+import microservices.book.multiplication.domain.MultiplicationResultAttempt;
+
+public interface MultiplicationService {
+
+    /* 두 개의 무작위 인수(11~99)를 담은 {@link Multiplication} 객체를 생성 */
+    Multiplication createRandomMultiplication();
+
+    /* 곱셈 결과가 맞으면 true, 아니면 false */
+    boolean checkAttempt(final MultiplicationResultAttempt resultAttempt);
+}
